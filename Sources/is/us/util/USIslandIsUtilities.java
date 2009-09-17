5b0a7ffb96f0efb9dc6e01caf98d34617fea8dbd
Atli Páll Hafsteinsson <atlipall@gmail.com>
2009-09-17 13:37:01 +0000

    INN-719 USIslandIsSetting renamed to USIslandIsFactory client creation static method moved from USIslandIsUtilities to USIslandIsFactory certifications insertation to system props moved to USIslandIsAuthenticationClient

--- Sources/is/us/util/USIslandIsUtilities.java
+++ Sources/is/us/util/USIslandIsUtilities.java
@@ -5,10 +5,9 @@ import is.us.wo.util.USHTTPUtilities;
 import org.slf4j.*;
 
 import com.webobjects.appserver.*;
-import com.webobjects.foundation.NSBundle;
 
 /**
- * A WO wrapper for {@link is.us.util.USIslandIsAuthenticationClient}
+ * A WO utility methods for island.is authentication service
  * 
  * @author Bjarni Sævarsson <bjarnis@us.is>
  * @author Atli Páll Hafsteinsson <atlip@us.is>
@@ -31,34 +30,16 @@ public class USIslandIsUtilities {
 	private static final String BASE_LINK = "https://www.island.is/audkenning/audkenning";
 
 	/**
-	 * @param request the {@link WORequest} for this session
-	 * @param keystorePath the path to the key store file
-	 * @param keystorePassword the password for the key store
-	 * @param username the island.is authentication service username
-	 * @param password the island.is authentication service password
-	 */
-	@SuppressWarnings( "deprecation" )
-	public static USIslandIsAuthenticationClient client( WORequest request, String keystorePath, String keystorePassword, String username, String password ) {
-
-		String keystoreFile = NSBundle.bundleForClass( USIslandIsUtilities.class ).pathForResource( keystorePath, null );
-		logger.debug( "Using keystore file at: " + keystoreFile );
-		System.setProperty( "javax.net.ssl.trustStore", keystoreFile );
-		System.setProperty( "javax.net.ssl.trustStorePassword", keystorePassword );
-
-		return new USIslandIsAuthenticationClient( token( request ), userIp( request ), username, password );
-	}
-
-	/**
 	 * @return The token.
 	 */
-	private static String token( WORequest request ) {
+	public static String token( WORequest request ) {
 		return request.stringFormValueForKey( TOKEN_KEY );
 	}
 
 	/**
 	 * @return The remote user's IP address, or the localhost IP if no IP is found.
 	 */
-	private static String userIp( WORequest request ) {
+	public static String userIp( WORequest request ) {
 		String userIp = USHTTPUtilities.ipAddressFromRequest( request );
 
 		if( userIp == null ) {
