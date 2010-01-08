2fd5ae240fc40e92b2c947c51e60db731afa663f
Hugi Thordarson <hugi@karlmenn.is>
2010-01-08 18:08:35 +0000

    HTML fields now automagically acquire focus

--- Sources/is/us/components/USHTMLEditorSimple.java
+++ Sources/is/us/components/USHTMLEditorSimple.java
@@ -3,6 +3,7 @@ package is.us.components;
 import com.webobjects.appserver.WOContext;
 
 import er.extensions.components.ERXComponent;
+import er.extensions.foundation.ERXStringUtilities;
 
 /**
  * @author Hugi Þórðarson
@@ -10,9 +11,23 @@ import er.extensions.components.ERXComponent;
 
 public class USHTMLEditorSimple extends ERXComponent {
 
+	private String _uniqueID;
+
 	public String value;
 
 	public USHTMLEditorSimple( WOContext context ) {
 		super( context );
+	}
+
+	/**
+	 * Return a uniqueID for html elements
+	 */
+	public String uniqueID() {
+
+		if( _uniqueID == null ) {
+			_uniqueID = ERXStringUtilities.replaceStringByStringInString( ".", "_", context().elementID() );
+		}
+
+		return _uniqueID;
 	}
 }
