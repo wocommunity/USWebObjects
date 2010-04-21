1738b2fc14a72b345b30fd0bb415eb4dbaee7e95
Hugi Thordarson <hugi@karlmenn.is>
2010-04-21 10:36:10 +0000

    Added the humanReadableList method

--- Tests/is/us/util/TestUSArrayUtilities.java
+++ Tests/is/us/util/TestUSArrayUtilities.java
@@ -2,6 +2,8 @@ package is.us.util;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.*;
+
 import org.junit.Test;
 
 public class TestUSArrayUtilities {
@@ -34,5 +36,36 @@ public class TestUSArrayUtilities {
 
 		newArr = USArrayUtilities.resize( oldArr, -1 );
 		assertEquals( newArr.length, 0 );
+	}
+
+	@Test
+	public void humanReadableList() {
+		String expected;
+		String actual;
+
+		expected = "Hugi";
+		actual = USArrayUtilities.humanReadableList( list( "Hugi" ) );
+		assertEquals( expected, actual );
+
+		expected = "Kjartan og Strumparnir";
+		actual = USArrayUtilities.humanReadableList( list( "Kjartan", "Strumparnir" ) );
+		assertEquals( expected, actual );
+
+		expected = "Hugi, Kjartan og Strumparnir";
+		actual = USArrayUtilities.humanReadableList( list( "Hugi", "Kjartan", "Strumparnir" ) );
+		assertEquals( expected, actual );
+	}
+
+	/**
+	 * Constructs a list for testing. 
+	 */
+	private static List<?> list( Object... objects ) {
+		ArrayList<Object> a = new ArrayList<Object>();
+
+		for( Object o : objects ) {
+			a.add( o );
+		}
+
+		return a;
 	}
 }
