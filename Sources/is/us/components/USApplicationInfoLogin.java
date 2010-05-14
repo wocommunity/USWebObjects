57a8d2eb280e22c1f26a32a5a63d949dc96feeeb
Hugi Thordarson <hugi@karlmenn.is>
2010-05-14 13:03:47 +0000

    Added a property for the password to USApplicationInfo

--- Sources/is/us/components/USApplicationInfoLogin.java
+++ Sources/is/us/components/USApplicationInfoLogin.java
@@ -1,18 +1,21 @@
 package is.us.components;
 
+import is.us.util.USStringUtilities;
+
 import com.webobjects.appserver.*;
 import com.webobjects.foundation.NSArray;
 
 import er.extensions.components.ERXComponent;
 
 /**
- * 
+ * The login page for USApplicationInfo.
+ *  
  * @author Hugi Thordarson
  */
 
 public class USApplicationInfoLogin extends ERXComponent {
 
-	private static final String PASSWORD = "horsenose";
+	private static final String PASSWORD_PROPERTY_NAME = "USApplicationInfo.password";
 	public String password;
 
 	public USApplicationInfoLogin( WOContext context ) {
@@ -25,10 +28,19 @@ public class USApplicationInfoLogin extends ERXComponent {
 	}
 
 	public WOActionResults login() {
-		if( PASSWORD.equals( password ) ) {
+
+		if( passwordProperty() == null ) {
+			throw new RuntimeException( USStringUtilities.stringWithFormat( "The property {} must be set", PASSWORD_PROPERTY_NAME ) );
+		}
+
+		if( passwordProperty().equals( password ) ) {
 			return pageWithName( USApplicationInfo.class );
 		}
 
 		return context().page();
+	}
+
+	private static final String passwordProperty() {
+		return System.getProperty( PASSWORD_PROPERTY_NAME );
 	}
 }
