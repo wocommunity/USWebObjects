cec1a2280fde99310d3b76c6bc815d0eac29a128
Hugi Thordarson <hugi@karlmenn.is>
2010-06-16 14:22:51 +0000

    Adding methods to sort USSortables

--- Sources/is/us/util/USSortableUtilities.java
+++ Sources/is/us/util/USSortableUtilities.java
@@ -1,6 +1,8 @@
 package is.us.util;
 
-import java.util.Collection;
+import java.util.*;
+
+import com.webobjects.foundation.NSMutableArray;
 
 /**
  * Mixin for use in objects that implement the SWSortable interface.
@@ -20,7 +22,28 @@ public abstract class USSortableUtilities {
 	/**
 	 * Indicates that this element is the last in it's list. 
 	 */
-	public static boolean isLast( USSortable object, Collection siblings ) {
+	public static boolean isLast( USSortable object, Collection<? extends USSortable> siblings ) {
 		return object.sortNumber().intValue() == (siblings.size() - 1);
+	}
+
+	/**
+	 * Indicates that this element is the last in it's list. 
+	 */
+	public static boolean moveUp( USSortable object, Collection<? extends USSortable> siblings ) {
+		return object.sortNumber().intValue() == (siblings.size() - 1);
+	}
+
+	public static void changeSortOrder( USSortable object, NSMutableArray<USSortable> siblings, int offset ) {
+		int i = siblings.indexOf( object );
+		siblings.remove( i );
+		siblings.insertObjectAtIndex( object, i + offset );
+
+		// Go through all components and resort
+		Enumeration<USSortable> e = siblings.objectEnumerator();
+
+		while( e.hasMoreElements() ) {
+			USSortable sibling = e.nextElement();
+			sibling.setSortNumber( siblings.indexOf( sibling ) );
+		}
 	}
 }
