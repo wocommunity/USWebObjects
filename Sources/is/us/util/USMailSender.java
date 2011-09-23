83bd9f4fd3550f25688b22db631c22690c074130
Bjarni Sævarsson <bjarnisaevarsson@gmail.com>
2011-09-23 14:06:10 +0000

    INN-948 - Added composeEmailWithException and composeEmailWithAlternateTextAndAttachmentsWithException which both throw exceptions but composeEmail and composeEmailWithAlternateTextAndAttachments changed to cover functions for them. In the cover functions the exception are still logged and nothing thrown

--- Sources/is/us/util/USMailSender.java
+++ Sources/is/us/util/USMailSender.java
@@ -2,31 +2,18 @@ package is.us.util;
 
 import is.us.wo.util.USC;
 
-import java.util.Date;
-import java.util.Enumeration;
-import java.util.Properties;
-import java.util.Random;
+import java.util.*;
 
-import javax.activation.DataHandler;
-import javax.activation.FileDataSource;
-import javax.mail.Message;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.*;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.*;
 
 import com.webobjects.appserver.WOApplication;
-import com.webobjects.foundation.NSArray;
-import com.webobjects.foundation.NSData;
-import com.webobjects.foundation.NSDictionary;
-import com.webobjects.foundation.NSPathUtilities;
+import com.webobjects.foundation.*;
 
-import er.javamail.ERMailDataAttachment;
-import er.javamail.ERMailDeliveryPlainText;
+import er.javamail.*;
 
 /**
  * USMailSender is a very simple class for sending email.
@@ -62,75 +49,79 @@ public class USMailSender {
 	 * @param subject The message subject
 	 * @param plainTextBody The message's plain text body.
 	 * @param htmlBody The message's HTML body.
-	 * @param attachmentFilePaths An NSArray of Strings containing paths to
-	 *        files to attach to the letter.
+	 */
+	public static void composeEmailWithAlternateTextAndAttachmentsWithException( String from, NSArray<String> to, NSArray<String> cc, NSArray<String> bcc, String subject, String plaintextBody, String htmlBody, NSArray<String> attachmentFilePaths ) throws MessagingException {
+		Properties defaultProperties = new Properties();
+		defaultProperties.put( "mail.smtp.host", WOApplication.application().SMTPHost() );
+		javax.mail.Session defaultMailSession = javax.mail.Session.getDefaultInstance( defaultProperties, null );
+		MimeMessage msg = new MimeMessage( defaultMailSession );
+		msg.setSentDate( new Date() );
+
+		if( from != null )
+			msg.setFrom( new InternetAddress( from ) );
+
+		if( to != null )
+			msg.setRecipients( Message.RecipientType.TO, InternetAddress.parse( to.componentsJoinedByString( ", " ), false ) );
+
+		if( cc != null )
+			msg.setRecipients( Message.RecipientType.CC, InternetAddress.parse( cc.componentsJoinedByString( ", " ), false ) );
+
+		if( bcc != null )
+			msg.setRecipients( Message.RecipientType.BCC, InternetAddress.parse( bcc.componentsJoinedByString( ", " ), false ) );
+
+		if( subject != null )
+			msg.setSubject( subject, "iso-8859-1" );
+
+		// Create the message
+		MimeMultipart alt = new MimeMultipart( "alternative" );
+
+		// Insert plain text content
+
+		MimeBodyPart mbp = new MimeBodyPart();
+		if( plaintextBody == null )
+			plaintextBody = "";
+		mbp.setContent( plaintextBody, "text/plain; charset=\"iso-8859-1\"" );
+		alt.addBodyPart( mbp );
+
+		// Insert HTML content
+		if( USStringUtilities.stringHasValue( htmlBody ) ) {
+			MimeBodyPart mbp2 = new MimeBodyPart();
+			mbp2.setContent( htmlBody, "text/html; charset=\"iso-8859-1\"" );
+			alt.addBodyPart( mbp2 );
+		}
+
+		if( !USArrayUtilities.arrayHasObjects( attachmentFilePaths ) ) {
+			msg.setContent( alt );
+		}
+		else {
+			MimeMultipart mixed = new MimeMultipart( "mixed" );
+			MimeBodyPart wrap = new MimeBodyPart();
+			wrap.setContent( alt );
+			mixed.addBodyPart( wrap );
+
+			// Insert attachment
+			for( String attachmentFilePath : attachmentFilePaths ) {
+				if( USStringUtilities.stringHasValue( attachmentFilePath ) ) {
+					MimeBodyPart attachment = new MimeBodyPart();
+					FileDataSource source = new FileDataSource( attachmentFilePath );
+					attachment.setDataHandler( new DataHandler( source ) );
+					attachment.setFileName( NSPathUtilities.lastPathComponent( attachmentFilePath ) );
+					mixed.addBodyPart( attachment );
+				}
+			}
+
+			msg.setContent( mixed );
+		}
+
+		Transport.send( msg );
+	}
+
+	/**
+	 * Cover function for {@link composeEmailWithException}, here the exception is logged and nothing thrown
 	 */
 	public static void composeEmailWithAlternateTextAndAttachments( String from, NSArray<String> to, NSArray<String> cc, NSArray<String> bcc, String subject, String plaintextBody, String htmlBody, NSArray<String> attachmentFilePaths ) {
-
 		try {
-			Properties defaultProperties = new Properties();
-			defaultProperties.put( "mail.smtp.host", WOApplication.application().SMTPHost() );
-			javax.mail.Session defaultMailSession = javax.mail.Session.getDefaultInstance( defaultProperties, null );
-			MimeMessage msg = new MimeMessage( defaultMailSession );
-			msg.setSentDate( new Date() );
-
-			if( from != null )
-				msg.setFrom( new InternetAddress( from ) );
-
-			if( to != null )
-				msg.setRecipients( Message.RecipientType.TO, InternetAddress.parse( to.componentsJoinedByString( ", " ), false ) );
-
-			if( cc != null )
-				msg.setRecipients( Message.RecipientType.CC, InternetAddress.parse( cc.componentsJoinedByString( ", " ), false ) );
-
-			if( bcc != null )
-				msg.setRecipients( Message.RecipientType.BCC, InternetAddress.parse( bcc.componentsJoinedByString( ", " ), false ) );
-
-			if( subject != null )
-				msg.setSubject( subject, "iso-8859-1" );
-
-			// Create the message
-			MimeMultipart alt = new MimeMultipart( "alternative" );
-
-			// Insert plain text content
-
-			MimeBodyPart mbp = new MimeBodyPart();
-			if( plaintextBody == null )
-				plaintextBody = "";
-			mbp.setContent( plaintextBody, "text/plain; charset=\"iso-8859-1\"" );
-			alt.addBodyPart( mbp );
-
-			// Insert HTML content
-			if( USStringUtilities.stringHasValue( htmlBody ) ) {
-				MimeBodyPart mbp2 = new MimeBodyPart();
-				mbp2.setContent( htmlBody, "text/html; charset=\"iso-8859-1\"" );
-				alt.addBodyPart( mbp2 );
-			}
-
-			if( !USArrayUtilities.arrayHasObjects( attachmentFilePaths ) ) {
-				msg.setContent( alt );
-			}
-			else {
-				MimeMultipart mixed = new MimeMultipart( "mixed" );
-				MimeBodyPart wrap = new MimeBodyPart();
-				wrap.setContent( alt );
-				mixed.addBodyPart( wrap );
-
-				// Insert attachment
-				for( String attachmentFilePath : attachmentFilePaths ) {
-					if( USStringUtilities.stringHasValue( attachmentFilePath ) ) {
-						MimeBodyPart attachment = new MimeBodyPart();
-						FileDataSource source = new FileDataSource( attachmentFilePath );
-						attachment.setDataHandler( new DataHandler( source ) );
-						attachment.setFileName( NSPathUtilities.lastPathComponent( attachmentFilePath ) );
-						mixed.addBodyPart( attachment );
-					}
-				}
-
-				msg.setContent( mixed );
-			}
-
-			Transport.send( msg );
+			composeEmailWithAlternateTextAndAttachmentsWithException( from, to, cc, bcc, subject, plaintextBody, htmlBody, attachmentFilePaths );
 		}
 		catch( Exception e ) {
 			logger.error( "Failed to send email", e );
@@ -138,22 +129,7 @@ public class USMailSender {
 	}
 
 	/**
-	 * Composes an email message with the given parameters and sends it
-	 * immediately. You can specify either a plaintext message body, an HTML
-	 * message body or both.
-	 * 
-	 * Uses the default mail server specified in the SoloWeb system's settings.
-	 * 
-	 * @param from The senders email address
-	 * @param to An NSArray containing the recipients' email addresses as
-	 *        strings.
-	 * @param cc An NSArray containing the cc'd recipients' email addresses as
-	 *        strings.
-	 * @param bcc An NSArray containing the bcc'd recipients' email addresses as
-	 *        strings.
-	 * @param subject The message subject
-	 * @param plainTextBody The message's plain text body.
-	 * @param htmlBody The message's HTML body.
+	 * Cover function for composeEmailWithException, here the exception is logged and nothing thrown
 	 */
 	public static void composeEmail( String from, NSArray<String> to, NSArray<String> cc, NSArray<String> bcc, String subject, String plaintextBody, String htmlBody ) {
 		composeEmailWithAlternateTextAndAttachments( from, to, cc, bcc, subject, plaintextBody, htmlBody, null );
@@ -162,29 +138,36 @@ public class USMailSender {
 	/**
 	 * Send mail with multiple attachments.
 	 */
+	public static void composeEmailWithException( String fromEmail, NSArray<String> toAddresses, String subject, String content, NSDictionary<String, NSData> attachments ) throws AddressException, MessagingException {
+		ERMailDeliveryPlainText mail = new ERMailDeliveryPlainText();
+		mail.newMail();
+		mail.setFromAddress( fromEmail );
+		mail.setToAddresses( toAddresses );
+		mail.setSubject( subject );
+
+		if( !USStringUtilities.stringHasValue( content ) )
+			content = USC.EMPTY_STRING;
+
+		mail.setTextContent( content );
+
+		if( attachments != null ) {
+			Enumeration<String> allAttachmentNames = attachments.allKeys().objectEnumerator();
+
+			while( allAttachmentNames.hasMoreElements() ) {
+				String nextAttachmentName = allAttachmentNames.nextElement();
+				mail.addAttachment( new ERMailDataAttachment( nextAttachmentName, String.valueOf( RANDOM.nextInt() ), attachments.objectForKey( nextAttachmentName ) ) );
+			}
+		}
+
+		mail.sendMail();
+	}
+
+	/**
+	 * Cover function for composeEmailWithException, here the exception is logged and nothing thrown
+	 */
 	public static void composeEmail( String fromEmail, NSArray<String> toAddresses, String subject, String content, NSDictionary<String, NSData> attachments ) {
 		try {
-			ERMailDeliveryPlainText mail = new ERMailDeliveryPlainText();
-			mail.newMail();
-			mail.setFromAddress( fromEmail );
-			mail.setToAddresses( toAddresses );
-			mail.setSubject( subject );
-
-			if( !USStringUtilities.stringHasValue( content ) )
-				content = USC.EMPTY_STRING;
-
-			mail.setTextContent( content );
-
-			if( attachments != null ) {
-				Enumeration<String> allAttachmentNames = attachments.allKeys().objectEnumerator();
-
-				while( allAttachmentNames.hasMoreElements() ) {
-					String nextAttachmentName = allAttachmentNames.nextElement();
-					mail.addAttachment( new ERMailDataAttachment( nextAttachmentName, String.valueOf( RANDOM.nextInt() ), attachments.objectForKey( nextAttachmentName ) ) );
-				}
-			}
-
-			mail.sendMail();
+			composeEmailWithException( fromEmail, toAddresses, subject, content, attachments );
 		}
 		catch( Exception e ) {
 			logger.error( "Failed to send email...", e );
